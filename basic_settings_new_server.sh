e65d8322e49794becb361845ea73cb228c68f291
Sven Stegemann <sven@stegemann.de>
2024-12-15 14:48:27 +0100

    Add SSH config

--- basic_settings_new_server.sh
+++ basic_settings_new_server.sh
@@ -12,7 +12,8 @@ fi
 apt update
 apt upgrade --assume-yes
 apt install --assume-yes zsh sudo neovim
-useradd $USERNAME --create-home --shell /bin/zsh --groups sudo
+groupadd ssh_users
+useradd $USERNAME --create-home --shell /bin/zsh --groups sudo,ssh_users || usermod -aG sudo,ssh_users $USERNAME --shell /bin/zsh
 
 # Sudo without password for everyone in group 'sudo'
 awk '
@@ -20,6 +21,18 @@ awk '
 { print }
 ' /etc/sudoers > /etc/sudoers.new
 mv /etc/sudoers.new /etc/sudoers
+
+# Set SSH config
+awk '
+{ sub(/^#? ?PermitRootLogin.*$/, "PermitRootLogin no") }
+{ sub(/^#? ?PermitEmptyPasswords.*$/, "PermitEmptyPasswords no") }
+{ sub(/^#? ?X11Forwarding.*$/, "X11Forwarding no") }
+{ sub(/^#? ?AllowGroups.*$/, "AllowGroups ssh_users") }
+{print}
+' /etc/ssh/sshd_config > /etc/ssh/sshd_config.new
+mv /etc/ssh/sshd_config.new /etc/ssh/sshd_config
+grep AllowGroups /etc/ssh/sshd_config || echo 'AllowGroups ssh_users' >> /etc/ssh/sshd_config
+
 
 su $USERNAME -c "mkdir ~/.config ~/.ssh"
 su $USERNAME -c "echo $SSH_PUBKEY > ~/.ssh/authorized_keys"
